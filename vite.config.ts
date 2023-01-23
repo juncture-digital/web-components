9cd43e9adef98fe62d6363246b419342fea27db0
Ron Snyder <ron@snyderjr.com>
2023-01-23 07:56:02 -0500

    ghp testing

--- vite.config.ts
+++ vite.config.ts
@@ -31,66 +31,20 @@ export default defineConfig(({mode})=> {
           index: './index.html',
         },
         output: {
-          dir: 'docs',
+          dir: 'dist',
           assetFileNames: (assetInfo) => {
             let extType = assetInfo.name?.split('.').at(1)
             if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType || '')) extType = 'img';
             // return `assets/${extType}/[name]-[hash][extname]`;
             // return `assets/${extType}/[name]-${version}[extname]`;
-            return `assets/${extType}/[name][extname]`;
+            return `${extType}/[name][extname]`;
           },
           // entryFileNames: 'assets/js/[name]-[hash].js'
           // entryFileNames: `assets/js/[name]-${version}.js`,
-          entryFileNames: `assets/js/[name].js`,
-          chunkFileNames: 'assets/js/[name]-[hash].js'
+          entryFileNames: `js/[name].js`,
+          chunkFileNames: 'js/[name]-[hash].js'
         }
       }
     }
   }
 })
-
-/*
-// https://vitejs.dev/config/
-export default defineConfig(({mode})=>{
-
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-
-  plugins: [vue({
-    template: {
-      compilerOptions: {
-        isCustomElement: (tag) => {
-          return tag.startsWith('ve-') || tag.startsWith('sl-')
-        }
-      }
-    }
-  }
-
-  )],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name?.split('.').at(1)
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType || '')) extType = 'img';
-          // return `assets/${extType}/[name]-[hash][extname]`;
-          // return `assets/${extType}/[name]-${version}[extname]`;
-          return `assets/${extType}/[name][extname]`;
-        },
-        // entryFileNames: 'assets/js/[name]-[hash].js'
-        // entryFileNames: `assets/js/[name]-${version}.js`,
-        entryFileNames: `assets/js/[name].js`,
-        chunkFileNames: 'assets/js/[name]-[hash].js'
-      }
-    }
-  }
-
-})
-*/
