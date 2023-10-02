9f004a5c8df9c18cbcdf79e652f6d7a461808e51
Julia Ha <jha@RE550.home>
2023-10-02 13:57:54 -0400

    added updated menu auth component

--- src/components/MenuAuth.ce.vue
+++ src/components/MenuAuth.ce.vue
@@ -1,214 +1,214 @@
 <script setup lang="ts">
 
-   import { computed, ref, toRaw, watch, onMounted, nextTick } from 'vue'
-   // @ts-ignore
-   import { HSDropdown } from '../lib/preline/components/hs-dropdown'
+import { computed, onMounted, ref, toRaw, watch } from 'vue'
 
-  const props = defineProps({
-    background: { type: String },
-    position: { type: String, default: 'left' },
-    contact: { type: String },
-    contactFormTitle: { type: String },
-    contactSubject: { type: String }
+// @ts-ignore
+import { HSDropdown } from '../lib/preline/components/hs-dropdown'
+import netlifyIdentity from 'netlify-identity-widget'
+//import jwt_decode from 'jwt-decode'
+//import { useEntitiesStore } from '../store/entities'
+
+//import { storeToRefs } from 'pinia'
+
+const root = ref<HTMLElement | null>(null)
+const host = computed(() => (root.value?.getRootNode() as any)?.host)
+const shadowRoot = computed(() => root?.value?.parentNode as HTMLElement)
+watch(shadowRoot, (shadowRoot) => new HSDropdown(shadowRoot).init() )
+
+//const store = useEntitiesStore()
+
+watch(host, () => { getMenuItems() })
+
+const menuItems = ref<any[]>([])
+
+//const { isLoggedIn, user } = storeToRefs(store)
+let isLoggedIn = ref(false)
+let user = <any|null>null
+
+const props = defineProps({
+  logo: { type: String },
+  title: { type: String },
+  auth: { type: String },
+})
+
+function getMenuItems() {
+  function parseSlot() {
+    menuItems.value = Array.from(host.value.querySelectorAll('li'))
+      .map((li: any) => {
+        const a = li.querySelector('a') as HTMLAnchorElement
+        let label = a.innerText
+        let icon = li.querySelector('svg') as SVGElement
+        return { label, icon, href: a.href }
+      })
+    }
+    
+    parseSlot()
+    new MutationObserver(
+      (mutationsList:any) => {
+        for (let mutation of mutationsList) { if (mutation.type === 'childList') parseSlot() }      
+      }
+    ).observe(host.value, { childList: true, subtree: true })
+  }
+
+  onMounted(async () => {
+    //if (props.auth === 'netlify') setupNetlifyAuth()
+    if (props.auth === 'github') setupGithubAuth()
+    console.log('isLoggedIn mounted',isLoggedIn.value)
   })
 
+  watch(user, () => {
+    if (user.value) localStorage.setItem('user', JSON.stringify(user.value))
+    else if (localStorage.getItem('user')) localStorage.removeItem('user')
+  })
+
+  function login(evt:Event) {
+    evt.preventDefault()
+    //if (props.auth === 'netlify') netlifyIdentity.open('login')
+    isLoggedIn.value = true;
+    if (props.auth === 'github') ghLogin()
+  }
+
+  function logout(evt:Event) {
+    evt.preventDefault()
+    //store.setUser(null)
+    user.value = null
+    localStorage.setItem('user', JSON.stringify(user.value))
+    isLoggedIn.value = false
+    //if (props.auth === 'netlify') netlifyIdentity.logout()
+    if (props.auth === 'github') ghLogout()
+  }
+
+  function toggleLogin(evt:Event) {
+    evt.preventDefault()
+    isLoggedIn.value = !isLoggedIn.value
+    let menu = shadowRoot.value.querySelector('.hs-dropdown.open')?.querySelector('.hs-dropdown-menu') as HTMLElement
+    menu.classList.remove('block')
+    menu.classList.add('hidden')
+  }
+
+
+  /***************** Netlify auth *****************/
+  /*
+  let netlifyIdentityEndpoint = 'https://juncture-search.netlify.app/.netlify/identity'
+
+  function setupNetlifyAuth() {
+    if (location.hostname === 'search.plant-humanities.org') netlifyIdentityEndpoint = 'https://search.plant-humanities.org/.netlify/identity'
+    let _user: any = localStorage.getItem('auth-user') && JSON.parse(localStorage.getItem('auth-user') || '{}' )
+    if (_user?.provider === 'netlify') store.setUser(_user)
+    else store.setUser(null)
+    netlifyIdentity.on('init', _user => {
+      if (_user) store.setUser({provider: 'netlify', name: _user.user_metadata.full_name, email: _user.email, token: _user.token})
+    })
+    netlifyIdentity.on('error', err => console.error('Error', err))
+    netlifyIdentity.on('login', _user => {
+      store.setUser({provider: 'netlify', name: _user.user_metadata.full_name, email: _user.email, token: _user.token})
+      netlifyIdentity.close()
+    })
+    netlifyIdentity.init({ APIUrl: netlifyIdentityEndpoint})
+    validateNetlifyUser()
+  }
+
+  function validateNetlifyUser() {
+    let _user: any = localStorage.getItem('auth-user') && JSON.parse(localStorage.getItem('auth-user') || '{}' )
+    if (_user && !isLoggedIn.value) {
+      // keep users logged in
+      const formData = new FormData()
+      formData.append('grant_type', 'refresh_token')
+      formData.append('refresh_token', _user.token.refresh_token)
+      fetch(`${netlifyIdentityEndpoint}/token`, {
+        method : 'POST',
+        body : formData
+      }).then(x=>x.json()).then((newToken: any) => {
+        console.log('validateNetlifyUser: newToken', newToken)
+        _user.token.access_token = newToken.access_token
+        _user.token.refresh_token = newToken.refresh_token
+        _user.token.expires_at = (jwt_decode(newToken.access_token) as any).exp * 1000
+        store.setUser({provider: 'netlify', name: _user.user_metadata.full_name, email: _user.email, token: _user.token})
+      })
+      return null
+    }
+    return null
+  }
+  */
+
+  /***************** Github auth *****************/
+  
   const clientIds:any = {
     'www.juncture-digital.org': 'f7247e1e4769ba7c61e4',
-    'beta.juncture-digital.org': 'f30ce4168a0bb95ecaa3',
     'dev.juncture-digital.org': 'bb290b5a738cb6fe31c7',
-    'tools.juncture-digital.org': '0b40086fe912d97c03e6'
+    'search.juncture-digital.org': 'ce9f2d2fb0a3498569fd',
+    'lab.plant-humanities.org': '2883735f052415de88ce',
+    'beta.plant-humanities.org': '4304885bd2882ecd28cc',
+    'search.plant-humanities.org': 'e75df39d883077c1013f',
   }
 
-  const nav:any = {
-    about: {icon: 'info-circle-fill'},
-    contact: {icon: 'envelope-fill'},
-    documentation: {icon: 'book'},
-    docs: {icon: 'book'},
-    editor: {icon: 'pencil', loginRequired: true},
-    essays: {icon: 'pencil'},
-    help: {icon: 'question-circle'},
-    home: {icon: 'house-fill'},
-    'how to': {icon: 'book-fill'},
-    link: {icon: 'link-45deg'},
-    login: {icon: 'person-circle'},
-    logout: {icon: 'person-circle'},
-    markdown: {icon: 'markdown'},
-    media: {icon: 'images', loginRequired: true},
-    showcase: {icon: 'easel2'},
-    tools: {icon: 'tools', loginRequired: true}
-  }
-
-  const root = ref<HTMLElement | null>(null)
-  const host = computed(() => (root.value?.getRootNode() as any)?.host)
-  const shadowRoot = computed(() => root?.value?.parentNode)
-  watch(shadowRoot, () => { new HSDropdown(shadowRoot.value).init() })
-
-  const isLoggedIn = ref(false)
-  const originalNavItems = ref<any[]>([])
-
-  const navItems = ref<any[]>()
-  // watch(navItems, () => console.log('menu.navItems', toRaw(navItems.value)) )
-
-  let helpWindow:any
-  let externalWindow:any
-
-  onMounted(() => {
-    init()
-    nextTick(() => {
-      let listItems = Array.from(host.value.children[0]?.children || [])
-      if (listItems.length > 0)
-      originalNavItems.value = listItems
-        .map((navItem:any) => {
-          if (navItem.firstChild.nodeName === 'A') {
-            let linkEl = navItem.firstChild as HTMLLinkElement
-            return {label: linkEl.textContent, href:linkEl.href, newWindow: linkEl.getAttribute('target') === '_blank' }
-          } else {
-            let text = navItem.firstChild.textContent
-            if (text.toLowerCase() === 'auth') {
-              return {label:  isLoggedIn.value ? 'Logout' : 'Login', href:  isLoggedIn.value ? 'logout' : 'login'}
-            } else {
-               console.log('returning textContent', navItem.textContent)
-              return {label: navItem.textContent}
-            }
-          }
-        })
-        .filter(item => item.label.toLowerCase().indexOf('contact') !== 0 || props.contact)
-    })
-  })
-
-  function init() {    
-    isLoggedIn.value = ghAuthToken() !== null
+  function setupGithubAuth() {
+    let _user: any = localStorage.getItem('auth-user') && JSON.parse(localStorage.getItem('auth-user') || '{}' )
+    if (_user?.provider === 'github') user.value=(_user)
+    //else user.value={}
     let code = (new URL(window.location.href)).searchParams.get('code')
-    console.log('code', code);
     if (code) {
-      window.history.replaceState({}, '', window.location.pathname)
+      console.log('code', code)
+      let href = `${location.pathname}${location.hash}`
+      window.history.replaceState({}, '', href)
       let url = window.location.hostname === 'localhost'
-        ? `https://dev.juncture-digital.org/gh-token?code=${code}&hostname=${window.location.hostname}`
+        ? `https://dev.juncture-digital.org/gh-token?code=testing&hostname=${window.location.hostname}`
         : `/gh-token?code=${code}&hostname=${window.location.hostname}`
-      console.log(url)
       fetch(url)
         .then(resp => resp.text())
-        .then(authToken => {
-          if (authToken) {
-            isLoggedIn.value = true
-            localStorage.setItem('gh-auth-token', authToken)
-            window.dispatchEvent(new Event('storage'))
-          }
-        })
+        .then(token => { if (token) {getGhUserInfo(token); isLoggedIn.value = true;} })
         .catch(err => console.log('err', err))
     }
-    host.value.classList.add(props.position)
   }
 
-  watch(originalNavItems, () => {
-    navItems.value = originalNavItems.value
-      .filter(item => {
-        let action = item.href ? item.href.split('/').filter((pe:string) => pe).pop().toLowerCase() : 'link'
-        return !nav[action] || !nav[action]?.loginRequired || isLoggedIn.value
-      })
-      .map((item:any) => {
-        return item.label === 'Login' || item.label === 'Logout'
-          ? {label:  isLoggedIn.value ? 'Logout' : 'Login', href:  isLoggedIn.value ? 'logout' : 'login'}
-          : item
-        }
-      )
-  })
-
-  watch(isLoggedIn, () => {
-    navItems.value = originalNavItems.value
-      .filter(item => {
-        let action = item.href ? item.href.split('/').filter((pe:string) => pe).pop().toLowerCase() : 'link'
-        return !nav[action] || !nav[action]?.loginRequired || isLoggedIn.value
-      })
-      .map(item =>
-        item.label === 'Login' || item.label === 'Logout'
-          ?{ label:  isLoggedIn.value ? 'Logout' : 'Login', href:  isLoggedIn.value ? 'logout' : 'login'}
-          : item
-      )
-  })
-
-  function  ghAuthToken() {
-    return localStorage.getItem('gh-auth-token')
-  }
-
-  function showContactForm() {
-    let contactDialog = shadowRoot.value?.querySelector('ve-contact') as any
-    contactDialog._instance.exposed.show.value = true
-  }
-
-  function showHelpDialog() {
-    let helpDialog = shadowRoot.value?.querySelector('#help') as any
-    helpDialog.show = !helpDialog.show
-  }
-
-  function showHelpWindow() {
-    if (helpWindow) { helpWindow.close() }
-    let options = 'toolbar=yes,location=yes,left=0,top=0,width=1040,height=1200,scrollbars=yes,status=yes'
-    helpWindow = window.open('/help', '_blank', options)
-  }
-
-  function showMarkdownDialog() {
-    let markdownDialog = shadowRoot.value?.querySelector('#markdown') as any
-    markdownDialog.show = !markdownDialog.show
-  }
-
-  function login() {
+  function ghLogin() {
     let hostname = (new URL(window.location.href)).hostname
     let isDev = hostname === 'localhost' || hostname.indexOf('192.168.') === 0
+    let redirectTo = `${window.location.href}`
     let href = isDev
-      ? `${window.location.pathname}?code=testing`
+      ? `${window.location.pathname}?code=testing&redirect_uri=${location.pathname}${location.hash}`
       : clientIds[location.hostname] !== undefined
-        ? `https://github.com/login/oauth/authorize?client_id=${clientIds[location.hostname]}&scope=repo&state=juncture&redirect_uri=${location.href}`
+        ? `https://github.com/login/oauth/authorize?client_id=${clientIds[location.hostname]}&scope=repo&state=juncture&redirect_uri=${redirectTo}`
         : null
     console.log(`login: hostname=${hostname} isDev=${isDev} href=${href}`)
+    isLoggedIn.value = true
     if (href) window.location.href = href
+  
   }
 
-  function logout() {
-    console.log('logout')
+  function ghLogout() {
     Object.keys(localStorage).forEach(key => localStorage.removeItem(key))
-    window.dispatchEvent(new Event("storage"))
-    isLoggedIn.value = false
+    //store.setUser(null)
+    user.value={}
+    location.href = ''
   }
 
-  function navIcon(item: any) {
-    let iconName = ''
-    let menuLabel = item.label.toLowerCase()
-    Object.keys(nav).forEach(key => {
-      if (menuLabel.indexOf(key) >= 0) iconName = nav[key].icon
-    })
-    return iconName ? iconName : 'link'
-  }
-
-  function menuItemSelected(item: any) {
-    console.log('menuItemSelected', item)
-    let action = item.href ? item.href.split('/').pop().toLowerCase() : ''
-    console.log(`action=${action}`)
-    if ((action.indexOf('contact') > -1 || item.label.toLowerCase().indexOf('contact') === 0) && props.contact) {
-      showContactForm()
-    } else if (action === 'login') {
-      login()
-    } else if (action === 'logout') {
-      logout()
-    } else if (action === 'help') {
-      // this.showHelpDialog()
-      showHelpWindow()
-    } else if (action === 'markdown') {
-      showMarkdownDialog()
-    } else if (item.href) {
-      if (item.newWindow) {
-        if (externalWindow) { externalWindow.close() }
-        externalWindow = window.open(item.href, '_blank', 'toolbar=yes,location=yes,left=0,top=0,width=1024,height=1200,scrollbars=yes,status=yes')
-      } else if (item.href.indexOf('/window.') > 0) {
-        let funcName = item.href.split('/window.').pop();
-        (window as any)[funcName]()
-      } else {
-        location.href = item.href
+  function getGhUserInfo(token:string) {
+    return fetch('https://api.github.com/user' ,{
+      headers: {
+        Accept: 'application/vnd.github+json',
+        Authorization: `token ${token}`
       }
-    } else {
+    }).then(resp => resp.json())
+    .then(info => user.value={provider: 'github', username: info.login, 'name': info.name, email: info.email, 'gh-token': token})
+    .then(info => localStorage.setItem('user', token))
+    //.then(info => localStorage.setItems({'provider': 'github', 'username': info.login, 'name': info.name, 'email': info.email, 'gh-token':token}))
+}
+
+  /*
+  watch(githubClient, async () => {
+    if (isLoggedIn.value) {
+      let username = await githubClient.value?.user().then((userData:any) => userData.login)
+      await githubClient.value?.repos(username).then((repos:any[]) => {
+        if (!repos.find(repo => repo.name === 'essays')) githubClient.value?.createRepository({name:'essays', description:'Juncture visual essays'})
+        if (!repos.find(repo => repo.name === 'media')) githubClient.value?.createRepository({name:'media', description:'Juncture media'})
+      })
     }
-  }
+  })
+  */
+
 </script>
+
 <template>
 
   <nav class="hs-dropdown relative inline-flex" ref="root" >
@@ -227,19 +227,37 @@
     <div
       class="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-[15rem] bg-white shadow-md rounded-lg p-2 mt-2 dark:bg-gray-800 dark:border dark:border-gray-700" 
       aria-labelledby="hs-dropdown-custom-icon-trigger">
-      <a v-for="item in originalNavItems" :key="item.href" 
-        class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" 
+      <a v-for="item in menuItems" :key="item.href" 
+        class="flex items-center gap-x-2 py-2 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" 
         :href="item.href"
-        @click="menuItemSelected(item)"
       >
-        {{ item.label }}
+        <svg v-if="item.icon" v-html="item.icon.outerHTML" class="w-4 h-4 text-gray-500"></svg>
+        <span v-else class="w-4"></span>
+        <span v-html="item.label" class="font-medium"></span>
       </a>
+
+      <a v-if="props.auth && isLoggedIn.value"
+        class="flex items-center gap-x-2 py-2 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" 
+        href="javascript;;" 
+        @click="logout"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
+        <span class="font-medium">({{user.name || user.email}})</span> <span class="font-medium">Logout</span>
+      </a>
+      <a v-if="props.auth && !isLoggedIn.value"
+        class="flex items-center gap-x-2 py-2 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" 
+        href="javascript;;" 
+        @click="login"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z"/></svg>
+        <span class="font-medium">Login</span>
+      </a>
+
     </div>
 
   </nav>
 
 </template>
-  
 
 <style>
   @import '../tailwind.css';
