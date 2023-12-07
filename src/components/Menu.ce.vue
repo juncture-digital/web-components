ff7c7371c360697c1493461865a56074686f2a7a
Ron Snyder <ron@snyderjr.com>
2023-12-07 10:33:29 -0500

    Updated index.html for local dev demo

--- src/components/Menu.ce.vue
+++ src/components/Menu.ce.vue
@@ -16,7 +16,7 @@ watch(shadowRoot, (shadowRoot) => new HSDropdown(shadowRoot).init() )
 watch(host, () => { getMenuItems() })
 
 const menuItems = ref<any[]>([])
-watch(menuItems, () => console.log('menuItems', toRaw(menuItems.value)))
+// watch(menuItems, () => console.log('menuItems', toRaw(menuItems.value)))
 
 const props = defineProps({
   logo: { type: String },
@@ -69,7 +69,17 @@ function getMenuItems() {
     let action = item.href.split('/').filter((x:string) => x).pop().toLowerCase()
     action = location.host === action ? 'home' : action
     if (action === 'search') window.open(item.href, '_blank');
-    else location.href = item.href
+    else {
+      let href = new URL(item.href)
+      if (href.origin === location.origin) {
+        let baseurl = ((window as any)?.config || {})?.baseurl || '/'
+        let path = href.pathname
+        if (path.indexOf(baseurl) === -1) path = `${baseurl}${path.slice(1)}`
+        location.pathname = path
+      } else {
+        location.href = item.href
+      }
+    }
   }
 
   function titleCase(word:string) {
@@ -220,7 +230,7 @@ function getMenuItems() {
       aria-labelledby="hs-dropdown-custom-icon-trigger">
       <a v-for="item in menuItems" :key="item.href" 
         class="flex items-center gap-x-2 py-2 rounded-md font-sans text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" 
-        :href="item.href"
+        href="javascript;;" 
         @click="menuItemSelected(item, $event)"
       >
         <svg v-if="item.icon" v-html="item.icon.outerHTML" class="w-4 h-4 text-gray-500"></svg>
@@ -246,6 +256,14 @@ function getMenuItems() {
         <span class="font-medium">Login with {{ titleCase(props.auth) }}</span>
       </a>
       <!-- End Auth -->
+
+      <a v-if="isLoggedIn"
+        class="flex items-center gap-x-2 py-2 rounded-md text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" 
+        href="/juncture/editor" 
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"/></svg>
+        <span class="font-medium">Editor</span>
+      </a>
 
       <a v-if="props.contact"
         class="flex items-center gap-x-2 py-2 rounded-md font-sans text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
